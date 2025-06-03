cf900a3fc1ddf891f4195d59056937370dd77633
frederika452 <rikafrede6@gmail.com>
2025-06-03 09:21:17 +0700

    Add files via upload

---
+++ routes.py
@@ -0,0 +1,51 @@
+from flask import Blueprint, render_template, redirect, url_for, flash
+from flask_login import login_user, login_required, logout_user, current_user # type: ignore
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from .models import User
+from .forms import LoginForm, RegisterForm
+from . import db, login_manager
+
+main = Blueprint('main', __name__)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@main.route('/')
+def index():
+    return redirect(url_for('main.login'))
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect(url_for('main.dashboard'))
+        else:
+            flash('Invalid username or password')
+    return render_template('login.html', form=form)
+
+@main.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_pw = generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_pw)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('main.login'))
+    return render_template('register.html', form=form)
+
+@main.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', user=current_user)
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.login'))
